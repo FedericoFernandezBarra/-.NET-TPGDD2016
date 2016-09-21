ae3332a3334df06ee699d67889a641df5e1d5268
brianwolf <brian.d.lobo@gmail.com>
2016-09-21 20:54:30 -0300

    subo primera parte de la migracion

--- SQL/script_creacion_inicial.sql
+++ SQL/script_creacion_inicial.sql
@@ -184,7 +184,6 @@ create table XXX.TIPO_DOCUMENTO
 
 go
 
-
 create table XXX.USUARIO
 (
     id_usuario  numeric(10,0) identity(1,1) ,
@@ -232,7 +231,6 @@ create table XXX.ROL_POR_USUARIO
 
 go
 
-
 create table XXX.FUNCIONALIDAD
 (
     id_funcionalidad  numeric(10,0) identity(1,1),
@@ -270,7 +268,7 @@ go
 create table XXX.PERSONAL
 (
     id_personal  numeric(10,0) references XXX.USUARIO(id_usuario) ,
-    matricula   nvarchar(30) unique,
+    matricula   nvarchar(30) --unique,
 
     PRIMARY KEY (id_personal)
 )
@@ -278,11 +276,10 @@ go
 
 create table XXX.AFILIADO
 (
-    id_afiliado numeric(10,0)     identity(1,1) ,
+    id_afiliado numeric(10,0),
     estado_civil numeric(10,0) ,
     numero_familiar numeric(10,0),
-    cantidad_hijos smallint,
-    activo  bit
+    cantidad_hijos smallint
 
     PRIMARY KEY (id_afiliado)
     FOREIGN KEY (id_afiliado)             references XXX.USUARIO(id_usuario),
@@ -336,7 +333,7 @@ go
 create table XXX.TIPO_ESPECIALIDAD
 (
     id_tipo_especialidad  numeric(10,0) identity(1,1) ,
-    descripcion nvarchar(30)
+    descripcion nvarchar(255)
 
     PRIMARY KEY (id_tipo_especialidad)
 )
@@ -347,14 +344,13 @@ create table XXX.ESPECIALIDAD
 (
     id_especialidad  numeric(10,0) identity(1,1) ,
     tipo_especialidad numeric(10,0),
-    descripcion nvarchar(30),
+    descripcion nvarchar(255),
 
     PRIMARY KEY (id_especialidad),
     FOREIGN KEY (tipo_especialidad)                   references XXX.TIPO_ESPECIALIDAD(id_tipo_especialidad)
 )
 
 go
-
 
 create table XXX.ESPECIALIDAD_POR_PERSONAL
 (
@@ -550,8 +546,6 @@ create table XXX.PLAN_HISTORICO_AFILIADO
 go
 
 
-
-
 /********************************************************************************************************************************/
 /*FUNCION HASH Y TRIGGER PARA LA CONTRASEÑA*/
 /********************************************************************************************************************************/
@@ -597,14 +591,14 @@ as begin
 
 declare @id_insertado numeric(18,0);  
  
-  select @id_insertado = id  
+  select @id_insertado = id_usuario 
   from inserted
 
   if(update(pass))
   begin
     update XXX.USUARIO  
     set pass = XXX.fn_hashear_pass(pass)  
-    where id = @id_insertado 
+    where id_usuario = @id_insertado 
   end
  
 end
@@ -614,3 +608,140 @@ go
 /********************************************************************************************************************************/
 /*MIGRACION*/
 /********************************************************************************************************************************/
+
+--TIPO DOCUMENTO
+-------------------------------------------------------------------------------------------------------
+insert into XXX.TIPO_DOCUMENTO
+values ('DNI')
+
+go
+
+--ESTADO CIVIL
+-------------------------------------------------------------------------------------------------------
+insert into XXX.ESTADO_CIVIL
+values 
+	('SOLTERO/A'),
+	('CASADO/A'),
+	('VIUDO/A'),
+	('CONCUBINATO'),
+	('DIVORCIADO/A')
+
+go
+
+--USUARIOS
+-------------------------------------------------------------------------------------------------------
+
+--AFILIADO
+insert into XXX.USUARIO
+	select distinct
+		Paciente_Dni,
+		Paciente_Dni,
+		0,
+		0,
+		Paciente_Nombre,
+		UPPER(Paciente_Apellido),
+		1,
+		Paciente_Dni,
+		Paciente_Fecha_Nac,
+		UPPER(Paciente_Direccion),
+		Paciente_Telefono,
+		Paciente_Mail,
+		null	--no tiene sexo la tabla maestra
+	from gd_esquema.Maestra
+	where 
+		Consulta_Sintomas is null and
+		Compra_Bono_Fecha is null
+
+go
+
+--PERSONAL
+insert into XXX.USUARIO
+	select distinct
+		Medico_Dni,
+		Medico_Dni,
+		0,
+		0,
+		Medico_Nombre,
+		UPPER(Medico_Apellido),
+		1,
+		Medico_Dni,
+		Medico_Fecha_Nac,
+		UPPER(Medico_Direccion),
+		Medico_Telefono,
+		Medico_Mail,
+		null	--no tiene sexo la tabla maestra
+	from gd_esquema.Maestra
+	where 
+		Consulta_Sintomas is null and
+		Compra_Bono_Fecha is null
+
+go
+
+--AFILIADOS
+-------------------------------------------------------------------------------------------------------
+insert into XXX.AFILIADO
+	select distinct
+		U.id_usuario,
+		null,
+		null,
+		null
+	from gd_esquema.Maestra as M
+	inner join XXX.USUARIO as U 
+		on M.Paciente_Dni = U.documento
+	where 
+		M.Consulta_Sintomas is null and
+		M.Compra_Bono_Fecha is null
+
+go
+
+--PERSONAL
+-------------------------------------------------------------------------------------------------------
+insert into XXX.PERSONAL
+	select distinct
+		U.id_usuario,
+		null
+	from gd_esquema.Maestra as M
+	inner join XXX.USUARIO as U 
+		on M.Medico_Dni = U.documento
+	where 
+		M.Consulta_Sintomas is null and
+		M.Compra_Bono_Fecha is null
+
+go
+
+--TIPO ESPECIALIDAD
+-------------------------------------------------------------------------------------------------------
+alter table XXX.TIPO_ESPECIALIDAD
+add tipo_especialidad_codigo numeric(18,0)
+
+go
+
+insert into XXX.TIPO_ESPECIALIDAD
+	select distinct
+		UPPER(Tipo_Especialidad_Descripcion),
+		Tipo_Especialidad_Codigo
+	from gd_esquema.Maestra 
+	where 
+		Tipo_Especialidad_Codigo is not null
+
+go
+
+--ESPECIALIDAD
+-------------------------------------------------------------------------------------------------------
+alter table XXX.ESPECIALIDAD
+add especialidad_codigo numeric(18,0)
+
+go
+
+insert into XXX.ESPECIALIDAD
+	select distinct
+		T.id_tipo_especialidad,
+		UPPER(M.Especialidad_Descripcion),
+		M.Especialidad_Codigo
+	from gd_esquema.Maestra as M
+	inner join XXX.TIPO_ESPECIALIDAD as T
+		on T.tipo_especialidad_codigo = M.Tipo_Especialidad_Codigo
+	where 
+		Especialidad_Codigo is not null
+
+go
