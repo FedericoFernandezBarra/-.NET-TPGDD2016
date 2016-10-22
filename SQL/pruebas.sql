adae01e2b1a47e3c72ddee620c4b8fa76dcf893b
LeoAristarco <chucky_92@hotmail.com.ar>
2016-10-22 04:20:36 -0300

    Update pruebas.sql

--- SQL/pruebas.sql
+++ SQL/pruebas.sql
@@ -336,3 +336,25 @@ create table BEMVINDO.BONO
 )
 
 go
+
+
+insert into BEMVINDO.TIPO_DOCUMENTO
+values ('DNI')
+
+go
+
+insert into BEMVINDO.ESTADO_CIVIL
+values 
+    ('SOLTERO/A'),
+    ('CASADO/A'),
+    ('VIUDO/A'),
+    ('CONCUBINATO'),
+    ('DIVORCIADO/A')
+
+go
+
+insert into BEMVINDO.PLAN_MEDICO(descripcion)
+values 
+    ('PLAN_POBREZA')
+
+go
