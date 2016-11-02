80ab3eafef495bba5aa48da2aec593f9bb6b901d
brianwolf <brian.d.lobo@gmail.com>
2016-11-02 19:20:50 -0300

    agrego validacion de la fecha con el dia de hoy

--- Registrar Agenta Medico/RegistrarAgendaForm.cs
+++ Registrar Agenta Medico/RegistrarAgendaForm.cs
@@ -147,6 +147,12 @@ namespace ClinicaFrba.Registrar_Agenta_Medico
                 return false;
             }
 
+            if (timerFechaDesde.Value.Date < DateTime.Now || timerFechaHasta.Value.Date < DateTime.Now)
+            {
+                MessageBox.Show("Las fechas ingresadas son anteriores a las del dia de hoy", "Error", MessageBoxButtons.OK);
+                return false;
+            }
+
             return true;
         }
 
