a9f03e87b8dc3731d066c4d7a804f285e4b7006d
LeoAristarco <chucky_92@hotmail.com.ar>
2016-11-06 16:19:15 -0300

    arreglo lo de comprar bono

--- SQL/script_creacion_inicial.sql
+++ SQL/script_creacion_inicial.sql
@@ -650,8 +650,8 @@ create table BEMVINDO.COMPRA
 (
     id_compra  numeric(10,0) identity(1,1),
     comprador  numeric(10,0) ,
-    cantidad numeric(3,0) ,
-    monto  float,--numeric(5,2),
+    cantidad numeric(10,0) ,
+    monto  numeric(10,2),
     fecha_compra      datetime,
 
     PRIMARY KEY (id_compra), 
@@ -1453,14 +1453,13 @@ go
 CREATE procedure BEMVINDO.st_insertar_compra
 
 @comprador numeric(10,0),
-@cantidad numeric(3,0),
-@monto    float,--numeric(5,2),
+@cantidad numeric(10,0),
+@monto    numeric(10,2),
 @fecha_compra  datetime,
 
 @id_compra numeric(10,0) output
 AS
 begin
---SELECT  CAST(@monto as decimal(5,2))
 
         insert into BEMVINDO.COMPRA(comprador,cantidad,monto,fecha_compra )
         values (@comprador,@cantidad,@monto,@fecha_compra)
