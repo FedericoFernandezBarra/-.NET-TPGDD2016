839a3deecf2ca1d80a0dd4c6b2fe3ad16ee0f8c8
alexiscaspell <alexiscaspell@gmail.com>
2016-12-10 18:02:43 -0300

    Hago que se puedan modificar tamaño de las columnas en listado estadistico

--- Listados/ListadoEstadisticoForm.Designer.cs
+++ Listados/ListadoEstadisticoForm.Designer.cs
@@ -53,9 +53,10 @@
             // 
             // limpiarBoton
             // 
-            this.limpiarBoton.Location = new System.Drawing.Point(9, 359);
+            this.limpiarBoton.Location = new System.Drawing.Point(12, 442);
+            this.limpiarBoton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.limpiarBoton.Name = "limpiarBoton";
-            this.limpiarBoton.Size = new System.Drawing.Size(134, 27);
+            this.limpiarBoton.Size = new System.Drawing.Size(179, 33);
             this.limpiarBoton.TabIndex = 7;
             this.limpiarBoton.Text = "Limpiar";
             this.limpiarBoton.UseVisualStyleBackColor = true;
@@ -68,9 +69,11 @@
             this.groupBox2.Controls.Add(this.mesLabel);
             this.groupBox2.Controls.Add(this.filtroEspecial);
             this.groupBox2.Controls.Add(this.tablaEstadistica);
-            this.groupBox2.Location = new System.Drawing.Point(10, 171);
+            this.groupBox2.Location = new System.Drawing.Point(13, 210);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(506, 183);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(675, 225);
             this.groupBox2.TabIndex = 24;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "TOP 5:";
@@ -80,9 +83,10 @@
             this.filtro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.filtro.Enabled = false;
             this.filtro.FormattingEnabled = true;
-            this.filtro.Location = new System.Drawing.Point(96, 16);
+            this.filtro.Location = new System.Drawing.Point(128, 20);
+            this.filtro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.filtro.Name = "filtro";
-            this.filtro.Size = new System.Drawing.Size(156, 21);
+            this.filtro.Size = new System.Drawing.Size(207, 24);
             this.filtro.TabIndex = 5;
             this.filtro.SelectedIndexChanged += new System.EventHandler(this.filtro_SelectedIndexChanged);
             // 
@@ -91,44 +95,47 @@
             this.mes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.mes.Enabled = false;
             this.mes.FormattingEnabled = true;
-            this.mes.Location = new System.Drawing.Point(343, 16);
+            this.mes.Location = new System.Drawing.Point(457, 20);
+            this.mes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mes.Name = "mes";
-            this.mes.Size = new System.Drawing.Size(104, 21);
+            this.mes.Size = new System.Drawing.Size(137, 24);
             this.mes.TabIndex = 4;
             // 
             // mesLabel
             // 
             this.mesLabel.AutoSize = true;
-            this.mesLabel.Location = new System.Drawing.Point(294, 19);
+            this.mesLabel.Location = new System.Drawing.Point(392, 23);
+            this.mesLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.mesLabel.Name = "mesLabel";
-            this.mesLabel.Size = new System.Drawing.Size(30, 13);
+            this.mesLabel.Size = new System.Drawing.Size(38, 17);
             this.mesLabel.TabIndex = 16;
             this.mesLabel.Text = "Mes:";
             // 
             // filtroEspecial
             // 
             this.filtroEspecial.AutoSize = true;
-            this.filtroEspecial.Location = new System.Drawing.Point(6, 24);
+            this.filtroEspecial.Location = new System.Drawing.Point(8, 30);
+            this.filtroEspecial.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.filtroEspecial.Name = "filtroEspecial";
-            this.filtroEspecial.Size = new System.Drawing.Size(0, 13);
+            this.filtroEspecial.Size = new System.Drawing.Size(0, 17);
             this.filtroEspecial.TabIndex = 14;
             // 
             // tablaEstadistica
             // 
             this.tablaEstadistica.AllowUserToAddRows = false;
             this.tablaEstadistica.AllowUserToDeleteRows = false;
-            this.tablaEstadistica.AllowUserToResizeColumns = false;
             this.tablaEstadistica.AllowUserToResizeRows = false;
             this.tablaEstadistica.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.tablaEstadistica.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.tablaEstadistica.BackgroundColor = System.Drawing.SystemColors.Window;
             this.tablaEstadistica.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.tablaEstadistica.GridColor = System.Drawing.SystemColors.Window;
-            this.tablaEstadistica.Location = new System.Drawing.Point(6, 48);
+            this.tablaEstadistica.Location = new System.Drawing.Point(8, 59);
+            this.tablaEstadistica.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tablaEstadistica.Name = "tablaEstadistica";
             this.tablaEstadistica.ReadOnly = true;
             this.tablaEstadistica.RowHeadersVisible = false;
-            this.tablaEstadistica.Size = new System.Drawing.Size(494, 123);
+            this.tablaEstadistica.Size = new System.Drawing.Size(659, 151);
             this.tablaEstadistica.TabIndex = 11;
             // 
             // groupBox1
@@ -141,9 +148,11 @@
             this.groupBox1.Controls.Add(this.TrimestreLabel);
             this.groupBox1.Controls.Add(this.anio);
             this.groupBox1.Controls.Add(this.anioLabel);
-            this.groupBox1.Location = new System.Drawing.Point(10, 11);
+            this.groupBox1.Location = new System.Drawing.Point(13, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(506, 154);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(675, 190);
             this.groupBox1.TabIndex = 23;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filtros de Búsqueda";
@@ -151,17 +160,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(133, 121);
+            this.label1.Location = new System.Drawing.Point(177, 149);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(250, 13);
+            this.label1.Size = new System.Drawing.Size(331, 17);
             this.label1.TabIndex = 19;
             this.label1.Text = "Los campos marcados con (*) son OBLIGATORIOS";
             // 
             // buscarButton
             // 
-            this.buscarButton.Location = new System.Drawing.Point(415, 116);
+            this.buscarButton.Location = new System.Drawing.Point(553, 143);
+            this.buscarButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buscarButton.Name = "buscarButton";
-            this.buscarButton.Size = new System.Drawing.Size(75, 23);
+            this.buscarButton.Size = new System.Drawing.Size(100, 28);
             this.buscarButton.TabIndex = 6;
             this.buscarButton.Text = "Buscar";
             this.buscarButton.UseVisualStyleBackColor = true;
@@ -172,18 +183,20 @@
             this.tipoListado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.tipoListado.Enabled = false;
             this.tipoListado.FormattingEnabled = true;
-            this.tipoListado.Location = new System.Drawing.Point(136, 39);
+            this.tipoListado.Location = new System.Drawing.Point(181, 48);
+            this.tipoListado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tipoListado.Name = "tipoListado";
-            this.tipoListado.Size = new System.Drawing.Size(354, 21);
+            this.tipoListado.Size = new System.Drawing.Size(471, 24);
             this.tipoListado.TabIndex = 1;
             this.tipoListado.SelectedIndexChanged += new System.EventHandler(this.tipoListado_SelectedIndexChanged);
             // 
             // TipoListadoLabel
             // 
             this.TipoListadoLabel.AutoSize = true;
-            this.TipoListadoLabel.Location = new System.Drawing.Point(37, 39);
+            this.TipoListadoLabel.Location = new System.Drawing.Point(49, 48);
+            this.TipoListadoLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TipoListadoLabel.Name = "TipoListadoLabel";
-            this.TipoListadoLabel.Size = new System.Drawing.Size(96, 13);
+            this.TipoListadoLabel.Size = new System.Drawing.Size(129, 17);
             this.TipoListadoLabel.TabIndex = 4;
             this.TipoListadoLabel.Text = "Tipo de Listado (*):";
             // 
@@ -192,26 +205,29 @@
             this.semestre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.semestre.Enabled = false;
             this.semestre.FormattingEnabled = true;
-            this.semestre.Location = new System.Drawing.Point(343, 77);
+            this.semestre.Location = new System.Drawing.Point(457, 95);
+            this.semestre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.semestre.Name = "semestre";
-            this.semestre.Size = new System.Drawing.Size(147, 21);
+            this.semestre.Size = new System.Drawing.Size(195, 24);
             this.semestre.TabIndex = 3;
             this.semestre.SelectedIndexChanged += new System.EventHandler(this.semestre_SelectedIndexChanged);
             // 
             // TrimestreLabel
             // 
             this.TrimestreLabel.AutoSize = true;
-            this.TrimestreLabel.Location = new System.Drawing.Point(274, 80);
+            this.TrimestreLabel.Location = new System.Drawing.Point(365, 98);
+            this.TrimestreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TrimestreLabel.Name = "TrimestreLabel";
-            this.TrimestreLabel.Size = new System.Drawing.Size(67, 13);
+            this.TrimestreLabel.Size = new System.Drawing.Size(91, 17);
             this.TrimestreLabel.TabIndex = 2;
             this.TrimestreLabel.Text = "Semestre (*):";
             // 
             // anio
             // 
-            this.anio.Location = new System.Drawing.Point(136, 77);
+            this.anio.Location = new System.Drawing.Point(181, 95);
+            this.anio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.anio.Name = "anio";
-            this.anio.Size = new System.Drawing.Size(100, 20);
+            this.anio.Size = new System.Drawing.Size(132, 22);
             this.anio.TabIndex = 2;
             this.anio.TextChanged += new System.EventHandler(this.anio_TextChanged);
             this.anio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.anio_KeyPress);
@@ -219,18 +235,20 @@
             // anioLabel
             // 
             this.anioLabel.AutoSize = true;
-            this.anioLabel.Location = new System.Drawing.Point(37, 80);
+            this.anioLabel.Location = new System.Drawing.Point(49, 98);
+            this.anioLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.anioLabel.Name = "anioLabel";
-            this.anioLabel.Size = new System.Drawing.Size(42, 13);
+            this.anioLabel.Size = new System.Drawing.Size(56, 17);
             this.anioLabel.TabIndex = 0;
             this.anioLabel.Text = "Año (*):";
             // 
             // volverButton
             // 
             this.volverButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.volverButton.Location = new System.Drawing.Point(382, 359);
+            this.volverButton.Location = new System.Drawing.Point(509, 442);
+            this.volverButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.volverButton.Name = "volverButton";
-            this.volverButton.Size = new System.Drawing.Size(134, 27);
+            this.volverButton.Size = new System.Drawing.Size(179, 33);
             this.volverButton.TabIndex = 8;
             this.volverButton.Text = "< < Volver";
             this.volverButton.UseVisualStyleBackColor = true;
@@ -238,16 +256,16 @@
             // 
             // ListadoEstadisticoForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 394);
+            this.ClientSize = new System.Drawing.Size(701, 485);
             this.Controls.Add(this.limpiarBoton);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.volverButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "ListadoEstadisticoForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
